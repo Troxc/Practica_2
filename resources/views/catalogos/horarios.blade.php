5254a5551ca856625808c27286bcfb1aa5ad7392
Carlos <carlos.eo199927@gmail.com>
2024-10-14 15:38:03 -0600

    Pequenas modificaciones

--- resources/views/catalogos/horarios.blade.php
+++ resources/views/catalogos/horarios.blade.php
@@ -1,6 +1,7 @@
 @extends('plantillas.plantilla')
 
 @section('contenido')
+    <h2 class="ms-3">Horarios</h2>
     <div class="list-group list-group-horizontal d-flex p-3">
         <a href="#" class="list-group-item list-group-item-primary list-group-item-action">Docentes</a>
         <a href="#" class="list-group-item list-group-item-primary list-group-item-action">Alumnos</a>
