29c989b42c331738342dcb84079f2b706d1495b9
Carlos <carlos.eo199927@gmail.com>
2024-10-15 16:28:51 -0600

    Cambios Reformados

--- resources/views/catalogos/catalogos.blade.php
+++ resources/views/catalogos/catalogos.blade.php
@@ -1,7 +1,6 @@
 @extends('plantillas.plantilla')
 
 @section('contenido')
-    <h2>Catalogos</h2>
     <div class="list-group col-2 mt-3">
         <a href="#" class="list-group-item list-group-item-primary list-group-item-action">Periodos</a>
         <a href="#" class="list-group-item list-group-item-primary list-group-item-action">Plazas</a>
--- resources/views/catalogos/proyectos.blade.php
+++ resources/views/catalogos/proyectos.blade.php
@@ -1,7 +1,6 @@
 @extends('plantillas.plantilla')
 
 @section('contenido')
-    <h2 class="ms-3">Proyectos Indv</h2>
     <div class="list-group list-group-horizontal d-flex p-3">
         <a href="#" class="list-group-item list-group-item-primary list-group-item-action">Capacitacion</a>
         <a href="#" class="list-group-item list-group-item-primary list-group-item-action">Asesorias Doc</a>
